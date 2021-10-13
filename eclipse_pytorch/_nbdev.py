46bbf86c3f696da63abe44b6e560cec789eb77c1
Thomas Capelle <thomas.capelle@steady-sun.com>
2021-10-13 10:58:08 +0200

    missing exported temporal block

--- eclipse_pytorch/_nbdev.py
+++ eclipse_pytorch/_nbdev.py
@@ -3,6 +3,7 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"SpatialDownsampler": "00_model.ipynb",
+         "TemporalModel": "00_model.ipynb",
          "Upsampler": "00_model.ipynb",
          "IrradianceModule": "00_model.ipynb",
          "Eclipse": "00_model.ipynb",
